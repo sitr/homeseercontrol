102e86f7eb0231b9bf5ab11edfbbd34da7e99cde
Sigbjørn Trageton <sigbjorn@trageton.net>
2018-04-23 21:25:33 +0200

    Must use http in fetch url to avoid localhost

--- src/js/HsDeviceController.js
+++ src/js/HsDeviceController.js
@@ -3,8 +3,8 @@ import { getConfig } from '../config';
 
 function getDeviceInfoFromHomeSeer (params) {
    var config = getConfig();
-   const URL =  config.homeseerProxyUrl;
-   var result = fetch(URL + "/JSON?request=getstatus&ref=" + params.deviceId)
+   const HOMESEER_URL =  config.homeseerApiHost;
+   var result = fetch("http://" + HOMESEER_URL + "/JSON?request=getstatus&ref=" + params.deviceId)
       .then(response => response.json())
       .then(data => {
          return new HsDevice(data);
