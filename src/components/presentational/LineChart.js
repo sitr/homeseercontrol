d191aa330ee2979b972bd89960e7e80916a8cf38
Sigbjørn Trageton <sigbjorn@trageton.net>
2022-01-21 14:25:16 +0100

    Chart not loading data at start. Annotation test

--- src/components/presentational/LineChart.js
+++ src/components/presentational/LineChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Line } from 'react-chartjs-2';
 import 'chartjs-adapter-moment';
+//import annotationPlugin from 'chartjs-plugin-annotation';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -12,9 +13,39 @@ import {
     Legend,
     TimeScale,
     TimeSeriesScale
-  } from "chart.js";
-  
-const chartData = {
+} from "chart.js";
+
+// var ann = [];
+// var ann_labels = ["Nå"];
+
+// var annotations_array = ann.map(function (value, index) {
+//     return {
+//         type: 'line',
+//         id: 'vline' + index,
+//         mode: 'vertical',
+//         scaleID: 'x',
+//         value: value,
+//         endValue: 200,
+//         borderColor: 'red',
+//         borderWidth: 2,
+//         label: {
+//             enabled: true,
+//             position: "center",
+//             content: ann_labels[index]
+//         }
+//     }
+// });
+
+const LineChart = (props) => {
+    
+    ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale, TimeSeriesScale);
+    
+    function stringTo2dArray(string, d1, d2) {
+        return string.split(d1).map(function (x) { return x.split(d2) });
+    }
+
+    var listOfPrices;
+    const chartData = {
     labels: [],
     datasets: [
         {
@@ -28,14 +59,7 @@ const chartData = {
         }
     ]
 }
-
-const LineChart = (props) => {
-    function stringTo2dArray(string, d1, d2) {
-        return string.split(d1).map(function (x) { return x.split(d2) });
-    }
-
-    ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale, TimeSeriesScale);
-    var listOfPrices;
+    
     if (props.device.status != null) {
         var stripped = props.device.status.replace(/\søre/g, ";").replace(/\s/g, "");
         var stripped = stripped.substring(0, stripped.length - 1);
@@ -43,29 +67,32 @@ const LineChart = (props) => {
         chartData.datasets[0].data = [];
         chartData.labels = [];
         listOfPrices.forEach(element => {
-            const now = new Date();
-            now.setHours(parseInt(element[0].substring(0, element[0].indexOf("."))));
-            chartData.datasets[0].data.push({x: now, y: parseFloat(element[1].replace(/\,/g, "."))});
-            chartData.labels.push(now);
+            const currentTime = new Date();
+            currentTime.setHours(parseInt(element[0].substring(0, element[0].indexOf("."))));
+            currentTime.setMinutes(0);
+            currentTime.setSeconds(0);
+            chartData.datasets[0].data.push({ x: currentTime, y: parseFloat(element[1].replace(/\,/g, ".")) });
+            chartData.labels.push(currentTime);
         });
         
     }
+
     return (
         <div id={props.device.ref} className={props.className}>
             <Line
                 type="line"
                 data={chartData}
                 options={{
-                    title:{
-                        display:true,
-                        text:'Dagens strømpriser',
-                        fontSize:20
+                    title: {
+                        display: true,
+                        text: 'Dagens strømpriser',
+                        fontSize: 20
                     },
-                    legend:{
-                        display:true,
-                        position:'right'
+                    legend: {
+                        display: true,
+                        position: 'right'
                     },
-                    scales:{
+                    scales: {
                         x: {
                             type: 'time',
                             time: {
